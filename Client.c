a4ca8700f47f14656a4842e6306384730d9706d2
유수현 <youg4231998@naver.com>
2024-04-03 20:45:01 +0900

    2d array random bingo board

---
+++ Client.c
@@ -0,0 +1,138 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <time.h>
+
+#define BUF_SIZE 1024
+#define BOARD_SIZE 5
+void error_handling(char *message);
+void printBingo();
+void createBingoBoard();
+
+int bingoBoard[BOARD_SIZE][BOARD_SIZE];
+
+
+int main(int argc, char *argv[])
+{
+    int sock;
+    //빙고판
+    int bingoBoard[25];
+    //입력 숫자
+    int input;
+    int init = 0;
+    struct sockaddr_in serv_adr;
+
+    //gcc clint.c -o clint          
+    //./client 127.0.0.1 9091 //temp ip address
+
+    // if (argc != 3)
+    // {
+    //     printf("Usage: %s <IP> <PORT>\n", argv[0]);
+    // }
+
+    // sock = socket(PF_INET, SOCK_STREAM, 0);
+    // if (sock == -1)
+    // {
+    //     error_handling("socket() error");
+    // }
+ 
+    // memset(&serv_adr, 0, sizeof(serv_adr));
+    // serv_adr.sin_family = AF_INET; //tcpip
+    // serv_adr.sin_addr.s_addr = inet_addr(argv[1]); //save ip저장
+    // serv_adr.sin_port = htons(atoi(argv[2]));   //port 저장
+
+    // if (connect(sock, (struct sockaddr*)&serv_adr, sizeof(serv_adr)) == -1)
+    // {
+    //     error_handling("socket() error");
+    //     return 0;
+    // }
+    // else 
+    // {
+    //     puts("Connected--------->");
+    // }
+
+    // while (1)
+    // {
+    //     if (init == 0)
+    //     {
+    //         for (int i = 0; i < 25; i++)
+    //         {
+    //             scanf("%d", &input);
+    //             bingoBoard[i] = input;
+    //         }
+    //         init = 1;
+    //         printBingo(bingoBoard);
+    //     }
+        
+    // }
+    
+    // close(sock);
+    createBingoBoard();
+    printBingo();
+
+    return 0;
+}
+
+void createBingoBoard()
+{
+    //check dup
+    int randomNums[25];
+    int dup = 0;
+
+    srand((unsigned int )time(NULL));
+    //create 25 non-dup int
+    for (int i = 0; i < 25;)
+    {
+        int num = (rand() % 50) + 1;
+        for (int j = 0; j < 25; j++)
+        {
+            //check dup
+            if (randomNums[j] == num)
+            {
+                dup = 1;
+                break;
+            }
+        }
+         //no dup insert
+         if (dup == 0)
+         {
+            randomNums[i] = num;
+            i++;
+         }
+         dup = 0;
+    }
+    int index = 0;
+    for (int i = 0; i < BOARD_SIZE; i++)
+    {
+        for (int j = 0; j < BOARD_SIZE; j++)
+        {
+            bingoBoard[i][j] = randomNums[index];
+            index++;
+        }   
+    }
+    return;
+}
+
+void printBingo()
+{
+    for (int i = 0; i < BOARD_SIZE; i++)
+    {
+        for (int j = 0; j < BOARD_SIZE; j++)
+        {
+            printf("%4d ", bingoBoard[i][j]);
+        }   
+        printf("\n");
+    }
+    return;
+}
+
+
+void error_handling(char *message)
+{
+    fputs(message, stderr);
+    fputc('\n', stderr);
+    exit(1);
+}
